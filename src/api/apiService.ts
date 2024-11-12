46c4300a248ee52e6d6ac858b0d3fd8bc5bd3246
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-11-11 20:24:03 -0700

    Conectar exitosamente la base de datos y el frontend

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -1,6 +1,8 @@
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const fetchData = async (): Promise<any> => {
+  console.log (backendUrl)
+
   try {
     const response = await fetch(`${backendUrl}/api/endpoint`, {
       method: 'GET',
