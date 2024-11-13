decbb8db2a117d3f80094eb83f84916cb521039d
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-11-12 23:51:06 -0700

    Avances de Navbar - Agregar logo y crear parte de Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 // src/App.tsx
+import logo from "./assets/oceanus-logo.svg";
 
 function App() {
   return (
-    <div className="w-full h-full absolute bg-gradient-to-r from-blue-400 to-emerald-400"></div>
-  )
-}
+    <div className="w-full h-full absolute bg-gradient-to-r from-blue-400 to-emerald-400">
+      <header className="flex justify-between items-center text-black py-3 px-4 md:px-20 bg-white drop-shadow-md">
+        <a href="#">
+          <img src={logo} alt="logo_oceanus" className="w-12 hover:scale-105 transition-all" />
+        </a>
+        <div className="relative hidden md:flex items-center justify-center gap-3">
+          <a>Administrador</a>
+        </div>
 
+      </header>
+    </div>
+  );
+}
 
 export default App;
