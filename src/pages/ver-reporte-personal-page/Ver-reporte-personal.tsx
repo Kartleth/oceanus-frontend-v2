53850c5c65da343f5235092a6fe66dedb20a1adb
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-11-27 18:20:51 -0700

    Terminar diseño para pantalla de reporte de empleado

--- src/pages/ver-reporte-personal-page/Ver-reporte-personal.tsx
+++ src/pages/ver-reporte-personal-page/Ver-reporte-personal.tsx
@@ -8,20 +8,19 @@ import {
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@radix-ui/react-separator";
 import Layout from "../../components/layout";
-import { FileUser, IdCard, Slash, UserRound } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+  BookUser,
+  FileUser,
+  GraduationCap,
+  Printer,
+  Slash,
+  Stethoscope,
+  UserRound,
+} from "lucide-react";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Label } from "@radix-ui/react-dropdown-menu";
-import { Link } from "react-router-dom";
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
+import { Button } from "@/components/ui/button";
 
 export default function VerReportePersonal() {
   return (
@@ -65,65 +64,108 @@ export default function VerReportePersonal() {
           <AvatarImage
             src="https://github.com/shadcn.png"
             alt="@shadcn"
-            className="w-32 rounded-full p-6"
+            className="w-60 rounded-full p-6"
           />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
-        <h1 className="font-medium text-2xl">Nombre de trabajador</h1>
+        <h1 className="font-medium text-2xl w-1/2">Nombre de trabajador</h1>
+        <div className="px-6 w-full flex justify-end">
+          <Button>
+            <Printer />
+            Imprimir reporte
+          </Button>
+        </div>
       </div>
 
       <div className="px-6">
         <hr />
       </div>
 
-      <div className="py-6 flex items-center">
-        <Card className="mx-auto max-w-sm w-1/2">
+      <div className="p-6 grid gap-6 md:grid-cols-2">
+        <Card className="w-full">
           <CardHeader>
-            <CardTitle className="text-2xl">Datos médicos</CardTitle>
+            <CardTitle className="text-2xl flex items-center gap-2">
+              <UserRound className="w-6 h-6" />
+              Datos personales
+            </CardTitle>
           </CardHeader>
+          <hr className="mb-4 border-softAqua" />
           <CardContent>
             <div className="grid gap-4">
               <div className="grid gap-2">
-                <Label>Dato médicos</Label>
+                <Label>Edad</Label>
+                <Label>Fecha de nacimiento</Label>
+                <Label>Género</Label>
+                <Label>Teléfono</Label>
+                <Label>Dirección</Label>
+                <Label>Licencia</Label>
+                <Label>Pasaporte</Label>
+                <Label>CURP</Label>
+                <Label>RFC</Label>
               </div>
             </div>
           </CardContent>
         </Card>
-        <Card className="mx-auto max-w-sm w-1/2">
-          <CardHeader>
-            <CardTitle className="text-2xl">Datos médicos</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <Label>Dato médicos</Label>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-      </div>
 
-      <div className="py-2 flex items-center">
-        <Card className="mx-auto max-w-sm w-1/2">
+        <Card className="w-full">
           <CardHeader>
-            <CardTitle className="text-2xl">Datos médicos</CardTitle>
+            <CardTitle className="text-2xl flex items-center gap-2">
+              <Stethoscope className="w-6 h-6" /> Datos médicos
+            </CardTitle>
           </CardHeader>
+          <hr className="mb-4 border-softAqua" />
           <CardContent>
             <div className="grid gap-4">
               <div className="grid gap-2">
-                <Label>Dato médicos</Label>
+                <Label>Alergias</Label>
+                <Label>Enfermedades</Label>
+                <Label>Lesiones</Label>
+                <Label>NSS</Label>
+                <Label>Contacto de emergencia</Label>
+                <Label>Relación</Label>
+                <Label>Número de emergencia</Label>
+                <Label>Tipo de sangre</Label>
               </div>
             </div>
           </CardContent>
         </Card>
-        <Card className="mx-auto max-w-sm w-1/2">
+
+        <Card className="w-full">
           <CardHeader>
-            <CardTitle className="text-2xl">Datos médicos</CardTitle>
+            <CardTitle className="text-2xl flex items-center gap-2">
+              <GraduationCap className="w-6 h-6" />
+              Datos académicos
+            </CardTitle>
           </CardHeader>
+          <hr className="mb-4 border-softAqua" />
           <CardContent>
             <div className="grid gap-4">
               <div className="grid gap-2">
-                <Label>Dato médicos</Label>
+                <Label>Carrera</Label>
+                <Label>Cédula</Label>
+                <Label>Experiencia laboral</Label>
+                <Label>Certificaciones</Label>
+                <Label>Grado de estudios</Label>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="text-2xl flex items-center gap-2">
+              <BookUser />
+              Datos de contratación
+            </CardTitle>
+          </CardHeader>
+          <hr className="mb-4 border-softAqua" />
+          <CardContent>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label>Estado</Label>
+                <Label>Tipo de contrato</Label>
+                <Label>Inicio de contrato</Label>
+                <Label>Fin de contrato</Label>
               </div>
             </div>
           </CardContent>
