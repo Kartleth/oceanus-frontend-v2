a77afe70e64c19696e86b84fc520be46e1ac510d
Kartleth <80503620+Kartleth@users.noreply.github.com>
2024-12-18 16:25:36 -0700

    Agregar QueryClientProvider a mi app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,34 @@ import { Page_personal } from "./pages/personal-page/personal-page";
 import { LoginForm } from "./components/login-form";
 import { VerDetallesPersonal } from "./pages/ver-detalles-personal-page/Ver-detalles-personal";
 import VerReportePersonal from "./pages/ver-reporte-personal-page/Ver-reporte-personal";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Page_personal />} />
-        <Route path="/personal" element={<Page_personal />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/contratos" element={<Contratos />} />
-        <Route path="/empresas" element={<Empresas />} />
-        <Route path="/facturas" element={<Facturas />} />
-        <Route path="/personal_terceros" element={<Terceros />} />
-        <Route path="/cotizaciones" element={<Cotizaciones />} />
-        <Route path="/detalles-trabajador/:id" element={<VerDetallesPersonal />} />
-        <Route path="/reporte-de-empleado/:id" element={<VerReportePersonal />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Page_personal />} />
+          <Route path="/personal" element={<Page_personal />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/contratos" element={<Contratos />} />
+          <Route path="/empresas" element={<Empresas />} />
+          <Route path="/facturas" element={<Facturas />} />
+          <Route path="/personal_terceros" element={<Terceros />} />
+          <Route path="/cotizaciones" element={<Cotizaciones />} />
+          <Route
+            path="/detalles-trabajador/:id"
+            element={<VerDetallesPersonal />}
+          />
+          <Route
+            path="/reporte-de-empleado/:id"
+            element={<VerReportePersonal />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
