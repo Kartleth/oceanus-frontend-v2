030034719bc02fad240802a2d63199f2314206c6
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-01-06 11:22:09 -0700

    Agregar diseño base a página para subir archivos

--- src/pages/subir-archivos-page/subir-archivos.tsx
+++ src/pages/subir-archivos-page/subir-archivos.tsx
@@ -1,5 +1,29 @@
-import { Layout } from "lucide-react";
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+} from "@/components/ui/breadcrumb";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-separator";
+import Layout from "../../components/Layout";
 
-export function SubirArchivos() {
-  return <Layout></Layout>;
+export default function SubirArchivos() {
+  return (
+    <Layout>
+      <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-gray-50 p-4">
+        <SidebarTrigger className="-ml-1" />
+        <Separator className="h-6 w-px bg-gray-300 mx-2" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/personal_terceros">
+                Subir archivos
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+    </Layout>
+  );
 }
