1149ffc0b6f643ea090e749b47fc3f6875af0890
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-01-06 11:45:33 -0700

    Agregar react-query a subir-archivos-tsx

--- src/pages/subir-archivos-page/subir-archivos.tsx
+++ src/pages/subir-archivos-page/subir-archivos.tsx
@@ -8,8 +8,23 @@ import {
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@radix-ui/react-separator";
 import Layout from "../../components/Layout";
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { Persona } from "@/modelos/personal";
+import axios from "axios";
 
 export default function SubirArchivos() {
+  const { id } = useParams();
+  const fetchEmpleado = async (): Promise<Persona> => {
+    const response = await axios.get(`http://localhost:3001/personas/${id}`);
+    return response.data;
+  };
+  const {
+    data: empleado,
+    isLoading,
+    isError, //lo dejare para cuando pongamos página de error
+    error,
+  } = useQuery<Persona>(["empleado", id], fetchEmpleado);
   return (
     <Layout>
       <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4">
