86dde661eb31557cbcb9204d3ed08564410f6173
ilsespinoza <104655979+ilsespinoza@users.noreply.github.com>
2025-01-08 22:47:29 -0700

    Los campos ya se cargan

--- src/pages/editar-trabajador/editar-trabajador.tsx
+++ src/pages/editar-trabajador/editar-trabajador.tsx
@@ -15,7 +15,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import {
@@ -50,20 +50,18 @@ type AccordionValue =
 export function PageEditarTrabajador() {
   const params = useParams();
   const id = params.id;
-  const query = useQuery({
+
+  const { data, isLoading } = useQuery({
     queryKey: ["trabajador", id],
     queryFn: async () => {
       const res = await fetch(`http://localhost:3001/personas/${id}`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
       });
       const resData = await res.json();
       if (!res.ok) {
         console.error(resData);
         throw new Error(resData.message);
       }
-      console.log(resData);
       const personaParse = Persona.safeParse(resData);
       if (!personaParse.success) {
         console.error(personaParse.error);
@@ -71,114 +69,69 @@ export function PageEditarTrabajador() {
       }
       return personaParse.data;
     },
-    onSuccess: (data) => {
-      datosPersonalesForm.reset(
-        {
-          nombre: data.nombre,
-          fechanacimiento: new Date(data.fechanacimiento),
-          curp: data.curp,
-          rfc: data.rfc,
-          ine: data.ine,
-          estadocivil: data.estadocivil,
-          numerofijo: data.numerofijo,
-          numerocelular: data.numerocelular,
-          correo: data.correo,
-          direccion: data.direccion,
-          numerolicencia: data.numerolicencia,
-          numeropasaporte: data.numeropasaporte,
-          fechaingreso: new Date(data.fechaingreso),
-        },
-        { keepValues: true }
-      );
-      datosAcademicosForm.reset(
-        {
-          cedula: data.datosAcademicos.cedula,
-          carrera: data.datosAcademicos.carrera,
-          certificaciones: data.datosAcademicos.certificaciones,
-          explaboral: data.datosAcademicos.explaboral,
-          gradoestudios: data.datosAcademicos.gradoestudios,
-        },
-        { keepValues: true }
-      );
-      datosMedicosForm.reset(
-        {
-          alergias: data.datosMedicos.alergias,
-          alergiasmed: data.datosMedicos.alergiasmed,
-          enfercronicas: data.datosMedicos.enfercronicas,
-          genero: data.datosMedicos.genero,
-          lesiones: data.datosMedicos.lesiones,
-          numemergencia: data.datosMedicos.numemergencia,
-          nombremergencia: data.datosMedicos.nombremergencia,
-          relaemergencia: data.datosMedicos.relaemergencia,
-          tiposangre: data.datosMedicos.tiposangre,
-          numseguro: data.datosMedicos.numseguro,
-        },
-        { keepValues: true }
-      );
-      datosContratacionForm.reset(
-        {
-          tipocontrato: data.tipocontrato,
-          estado: data.estado,
-          iniciocontrato: new Date(data.iniciocontrato),
-          fincontrato: new Date(data.fincontrato),
-        },
-        { keepValues: true }
-      );
-    },
-    refetchInterval: 5000,
   });
-
-  const [value, setValue] = useState<AccordionValue>("datos-personales"); //Mantiene el estado en un componente.
-  const datosContratacionForm = useForm<DatosContratacion>({
-    resolver: zodResolver(datosContratacionSchema),
-  });
-  function onSubmitCon(values: DatosContratacion) {
-    console.log(values);
-    guardarTrabajador();
-  }
-  const datosAcademicosForm = useForm<DatosAcademicos>({
-    resolver: zodResolver(datosAcademicosSchema),
-  });
-  function onSubmitAcd(values: DatosAcademicos) {
-    console.log(values);
-    setValue("datos-contratacion");
-  }
-  const datosMedicosForm = useForm<DatosMedicos>({
-    resolver: zodResolver(datosMedicosSchema),
-  });
-  function onSubmitMed(values: DatosMedicos) {
-    console.log(values);
-    setValue("datos-academicos");
-  }
 
   const datosPersonalesForm = useForm<DatosPersonales>({
     resolver: zodResolver(datosPersonalesSchema),
   });
-  function onSubmit(values: DatosPersonales) {
-    console.log(values);
-    setValue("datos-medicos");
+
+  const datosMedicosForm = useForm<DatosMedicos>({
+    resolver: zodResolver(datosMedicosSchema),
+  });
+
+  const datosAcademicosForm = useForm<DatosAcademicos>({
+    resolver: zodResolver(datosAcademicosSchema),
+  });
+
+  const datosContratacionForm = useForm<DatosContratacion>({
+    resolver: zodResolver(datosContratacionSchema),
+  });
+
+  const [value, setValue] = useState<AccordionValue>("datos-personales");
+
+  // Sincroniza los datos obtenidos con los formularios
+  useEffect(() => {
+    if (data) {
+      datosPersonalesForm.reset({
+        nombre: data.nombre,
+        fechanacimiento: new Date(data.fechanacimiento),
+        curp: data.curp,
+        rfc: data.rfc,
+        ine: data.ine,
+        estadocivil: data.estadocivil,
+        numerofijo: data.numerofijo,
+        numerocelular: data.numerocelular,
+        correo: data.correo,
+        direccion: data.direccion,
+        numerolicencia: data.numerolicencia,
+        numeropasaporte: data.numeropasaporte,
+        fechaingreso: new Date(data.fechaingreso),
+      });
+
+      datosMedicosForm.reset(data.datosMedicos);
+      datosAcademicosForm.reset(data.datosAcademicos);
+      datosContratacionForm.reset({
+        tipocontrato: data.tipocontrato,
+        estado: data.estado,
+        iniciocontrato: new Date(data.iniciocontrato),
+        fincontrato: new Date(data.fincontrato),
+      });
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
-  function guardarTrabajador() {
-    const datosPersonales = datosPersonalesForm.getValues();
-    const datosMedicos = datosMedicosForm.getValues();
-    const datosAcademicos = datosAcademicosForm.getValues();
-    const datosContratacion = datosContratacionForm.getValues();
-    const trabajador = {
-      datosMedicos: datosMedicos,
-      datosAcademicos: datosAcademicos,
-      datosPersonales: { ...datosPersonales, ...datosContratacion },
-    };
-    console.log(trabajador);
-  }
+
   return (
     <Layout>
-      <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4">
-        <SidebarTrigger className="-ml-1" />
+      <header className="sticky top-0 flex items-center gap-2 border-b bg-background p-4">
+        <SidebarTrigger />
         <Separator className="h-6 w-px bg-gray-300 mx-2" />
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
-              <BreadcrumbPage className="line-clamp-1 text-xl">
+              <BreadcrumbPage className="text-xl">
                 Editar Trabajador
               </BreadcrumbPage>
             </BreadcrumbItem>
@@ -193,47 +146,50 @@ export function PageEditarTrabajador() {
           onValueChange={setValue}
         >
           <AccordionItem value="datos-personales">
-            <AccordionTrigger className="[&[data-state=open]]:bg-gray-200 p-4 rounded-t-md transition-colors">
+            <AccordionTrigger className="bg-gray-100 text-gray-800 font-bold p-4 rounded-t-md border-b border-gray-300 transition-all hover:bg-gray-200">
               Datos Personales
             </AccordionTrigger>
-            <AccordionContent className="rounded-b-md bg-muted/50 p-4">
+            <AccordionContent className="p-4">
               <DatosPersonalesForm
                 form={datosPersonalesForm}
-                onSubmit={onSubmit}
-              ></DatosPersonalesForm>
+                onSubmit={() => setValue("datos-medicos")}
+              />
             </AccordionContent>
           </AccordionItem>
+
           <AccordionItem value="datos-medicos">
-            <AccordionTrigger className="[&[data-state=open]]:bg-gray-200 p-4 rounded-t-md transition-colors">
+            <AccordionTrigger className="bg-gray-100 text-gray-800 font-bold p-4 rounded-t-md border-b border-gray-300 transition-all hover:bg-gray-200">
               Datos Médicos
             </AccordionTrigger>
-            <AccordionContent className="rounded-b-md bg-muted/50 p-4">
+            <AccordionContent className="p-4">
               <DatosMedicosForm
                 form={datosMedicosForm}
-                onSubmitMed={onSubmitMed}
-              ></DatosMedicosForm>
+                onSubmitMed={() => setValue("datos-academicos")}
+              />
             </AccordionContent>
           </AccordionItem>
+
           <AccordionItem value="datos-academicos">
-            <AccordionTrigger className="[&[data-state=open]]:bg-gray-200 p-4 rounded-t-md transition-colors">
+            <AccordionTrigger className="bg-gray-100 text-gray-800 font-bold p-4 rounded-t-md border-b border-gray-300 transition-all hover:bg-gray-200">
               Datos Académicos
             </AccordionTrigger>
-            <AccordionContent className="rounded-b-md bg-muted/50 p-4">
+            <AccordionContent className="p-4">
               <DatosAcademicosForm
                 form={datosAcademicosForm}
-                onSubmitAcd={onSubmitAcd}
-              ></DatosAcademicosForm>
+                onSubmitAcd={() => setValue("datos-contratacion")}
+              />
             </AccordionContent>
           </AccordionItem>
+
           <AccordionItem value="datos-contratacion">
-            <AccordionTrigger className="[&[data-state=open]]:bg-gray-200 p-4 rounded-t-md transition-colors">
+            <AccordionTrigger className="bg-gray-100 text-gray-800 font-bold p-4 rounded-t-md border-b border-gray-300 transition-all hover:bg-gray-200">
               Datos de Contratación
             </AccordionTrigger>
-            <AccordionContent className="rounded-b-md bg-muted/50 p-4">
+            <AccordionContent className="p-4">
               <DatosContratacionForm
                 form={datosContratacionForm}
-                onSubmitCon={onSubmitCon}
-              ></DatosContratacionForm>
+                onSubmitCon={() => console.log("Guardar datos")}
+              />
             </AccordionContent>
           </AccordionItem>
         </Accordion>
