db58b6bcd39e529804125c21d895b69c6657d0ca
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-01-25 12:42:59 -0700

    Modificar claves para poder guardar correctamente un subcontratado

--- src/modelos/subcontratado.ts
+++ src/modelos/subcontratado.ts
@@ -1,13 +1,15 @@
 import { z } from "zod";
 
+export const EstadoSubcontratado = z.enum(["Activo", "Inactivo"]);
+
 export const Subcontratado = z.object({
   idsubcontratado: z.number(),
-  nombre: z.string(),
-  rfc: z.string(),
+  nombre: z.string().nullable(),
+  rfc: z.string().max(13),
   nss: z.string().nullable(),
-  ine: z.string(),
-  curp: z.string(),
-  estado: z.string(),
+  ine: z.string().nullable(),
+  curp: z.string().max(18),
+  estado: EstadoSubcontratado,
   doc: z.string().nullable(),
 });
 
