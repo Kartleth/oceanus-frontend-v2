34a9708f4d7f4bba96d0dd242465ef13c6c4407c
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-02-08 21:59:42 -0700

    Arreglar logo de sidebar

--- src/components/company-information.tsx
+++ src/components/company-information.tsx
@@ -22,12 +22,17 @@ export function CompanyInformation({
       <SidebarMenuItem>
         <SidebarMenuButton
           size="lg"
-          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          className="flex items-center space-x-1 data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
-          <div className="flex aspect-square size-12 items-center justify-center rounded-lg text-sidebar-primary-foreground">
-            <img src={activeTeam.logo} alt="Logo Oceanus DB" className="w-24"/>
-          </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
+          {/* Logo */}
+          <img
+            src={activeTeam.logo}
+            alt="Logo Oceanus DB"
+            className="h-auto w-[50px] transition-all duration-300 group-data-[collapsed=true]:w-[32px]"
+          />
+
+          {/* Texto - Se oculta cuando el sidebar está colapsado */}
+          <div className="flex flex-col text-start text-sm leading-tight transition-all duration-300 group-data-[collapsed=true]:hidden">
             <span className="truncate font-semibold">{activeTeam.name}</span>
             <span className="truncate text-xs">
               {activeTeam.fullNameCompany}
