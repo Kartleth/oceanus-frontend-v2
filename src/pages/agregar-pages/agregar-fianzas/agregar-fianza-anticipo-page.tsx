599aea8a22538489f754961be30fc06764910c87
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-03-23 16:24:43 -0700

    Agregar fianza anticipo-page

---
+++ src/pages/agregar-pages/agregar-fianzas/agregar-fianza-anticipo-page.tsx
@@ -0,0 +1,313 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import Layout from "@/components/Layout";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useForm } from "react-hook-form";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { useState } from "react";
+import { useMutation, useQueryClient } from "react-query";
+import { useNavigate } from "react-router-dom";
+import { datosContratacionSchema } from "@/components/forms/datos-trabajador-forms/datos-contratacion-form";
+import {
+  DatosGeneralesContratacion,
+  DatosGeneralesContratacionForm,
+  datosGeneralesSchema,
+} from "@/components/forms/datos-contratacion/datos-generales-form";
+import {
+  DatosFianzaCumplimientos,
+  datosFianzaCumplimientoSchema,
+  DatosFianzaCumplimientosForm,
+} from "@/components/forms/datos-contratacion/datos-fianza-cumplimiento-form";
+import {
+  DatosFianzaOcultos,
+  DatosFianzaOcultosForm,
+  datosFianzaOcultosSchema,
+} from "@/components/forms/datos-contratacion/datos-fianza-ocultos-form";
+import {
+  DatosFianzaAnticipos,
+  datosFianzaAnticipoSchema,
+  DatosFianzaAnticiposForm,
+} from "@/components/forms/datos-contratacion/datos-fianza-anticipo-form";
+
+type AccordionValue =
+  | "datos-anticipos"
+
+export function PageAgregarFianzaAnticipo() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const mutation = useMutation(async (data: any) => {
+    console.log(data);
+    const res = await fetch("http://localhost:3001/contrato", {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const resData = await res.json();
+    if (!res.ok) {
+      console.error(resData);
+    }
+    console.log(resData);
+    queryClient.invalidateQueries(["contratos"]);
+    navigate("/contratos");
+  });
+  const [value, setValue] = useState<AccordionValue>("datos-generales"); //Mantiene el estado en un componente.
+
+  const datosGeneralesForm = useForm<DatosGeneralesContratacion>({
+    resolver: zodResolver(datosGeneralesSchema),
+    defaultValues: {
+      anticipocontrato: "",
+      contratado: "",
+      convenio: "",
+      direccion: "",
+      fincontrato: new Date(),
+      idcontratofuente: "",
+      iniciocontrato: new Date(),
+      montocontrato: "",
+      nombrecontrato: "",
+      numerocontrato: "",
+      personalcontrato: "",
+      subcontrato: "",
+      tipocontrato: "",
+    },
+  });
+
+  function onSubmitGe(values: DatosGeneralesContratacion) {
+    console.log(values);
+    setValue("datos-cumplimiento");
+  }
+  const datosCumplimientoForm = useForm<DatosFianzaCumplimientos>({
+    resolver: zodResolver(datosFianzaCumplimientoSchema),
+  });
+  function onSubmitCum(values: DatosFianzaCumplimientos) {
+    console.log(values);
+    setValue("datos-ocultos");
+  }
+
+  const datosOcultosForm = useForm<DatosFianzaOcultos>({
+    resolver: zodResolver(datosFianzaOcultosSchema),
+  });
+  function onSubmitOcu(values: DatosFianzaOcultos) {
+    console.log(values);
+    setValue("datos-anticipos");
+  }
+
+  const datosAnticipoForm = useForm<DatosFianzaAnticipos>({
+    resolver: zodResolver(datosFianzaAnticipoSchema),
+  });
+  function onSubmitAntic(values: DatosFianzaAnticipos) {
+    console.log(values);
+    setValue("datos-anticipos");
+    guardarContratos();
+  }
+
+  async function formulariosSonValidos() {
+    if (!(await datosGeneralesForm.trigger())) {
+      setValue("datos-generales");
+
+      return false;
+    }
+    if (!(await datosCumplimientoForm.trigger())) {
+      setValue("datos-cumplimiento");
+
+      return false;
+    }
+    if (!(await datosOcultosForm.trigger())) {
+      setValue("datos-ocultos");
+
+      return false;
+    }
+    if (!(await datosAnticipoForm.trigger())) {
+      setValue("datos-anticipos");
+
+      return false;
+    }
+
+    return true;
+  }
+
+  async function guardarContratos() {
+    const validos = await formulariosSonValidos();
+
+    if (!validos) return;
+
+    const datosGenerales = datosGeneralesForm.getValues();
+    const datosAnticipo = datosAnticipoForm.getValues();
+    const datosCumplimiento = datosCumplimientoForm.getValues();
+    const datosOcultos = datosOcultosForm.getValues();
+    const agregarFianzaAnticipo =
+      datosAnticipo.tipodecambio &&
+      datosAnticipo.inicio &&
+      datosAnticipo.fin &&
+      datosAnticipo.monto;
+    const agregarFianzaCumplimiento =
+      datosCumplimiento.tipodecambio &&
+      datosCumplimiento.inicio &&
+      datosCumplimiento.fin &&
+      datosCumplimiento.monto;
+    const agregarFianzaOculto =
+      datosOcultos.tipodecambio &&
+      datosOcultos.inicio &&
+      datosOcultos.fin &&
+      datosOcultos.monto;
+    console.log("anticipo", agregarFianzaAnticipo);
+    console.log("anticipo", agregarFianzaCumplimiento);
+    console.log("anticipo", agregarFianzaOculto);
+    const contrato = {
+      nombreContrato: datosGenerales.nombrecontrato,
+      idContratado: datosGenerales.contratado,
+      personal: [],
+      tipoSubcontrato: datosGenerales.subcontrato,
+      iniciocontrato: datosGenerales.iniciocontrato,
+      fincontrato: datosGenerales.fincontrato,
+      convenio: [],
+      fianzaCumplimiento: !agregarFianzaCumplimiento
+        ? null
+        : {
+            // documento: datosCumplimiento.documento,
+            tipodecambio: datosCumplimiento.tipodecambio,
+            inicio: datosCumplimiento.inicio,
+            // anticipodoc: datosCumplimiento.anticipodoc,
+            fin: datosCumplimiento.fin,
+            poliza: datosCumplimiento.poliza,
+            aseguradora: datosCumplimiento.aseguradora,
+            monto: datosCumplimiento.monto,
+          },
+      fianzaOculto: !agregarFianzaOculto ? null : datosOcultos,
+      fianzaAnticipo: !agregarFianzaAnticipo ? null : datosAnticipo,
+      montoContrato: datosGenerales.montocontrato,
+      anticipoContrato: datosGenerales.anticipocontrato,
+      direccion: datosGenerales.direccion,
+      numeroContrato: datosGenerales.numerocontrato,
+      facturas: [],
+      ordenes: [],
+    };
+    console.log(contrato);
+    mutation.mutate(contrato);
+  }
+
+  const erroresGenerales = Object.keys(
+    datosGeneralesForm.formState.errors
+  ).length;
+
+  const erroresAnticipo = Object.keys(
+    datosAnticipoForm.formState.errors
+  ).length;
+  const erroresCumplimiento = Object.keys(
+    datosCumplimientoForm.formState.errors
+  ).length;
+  const erroresOculto = Object.keys(datosOcultosForm.formState.errors).length;
+
+  return (
+    <Layout>
+      <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4 bg-gray-50">
+        <SidebarTrigger className="-ml-1" />
+        <Separator className="h-6 w-px bg-gray-300 mx-2" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/contratos">Contratos</BreadcrumbLink>
+            </BreadcrumbItem>
+
+            <BreadcrumbSeparator />
+
+            <BreadcrumbItem>
+              <BreadcrumbLink href="editar-contratos">
+                Agregar contrato
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </header>
+      <main className="p-4">
+        <Accordion
+          type="single"
+          collapsible
+          value={value}
+          onValueChange={setValue}
+        >
+          <AccordionItem value="datos-generales">
+            <AccordionTrigger
+              data-haserrors={erroresGenerales > 0}
+              className="[&[data-state=open]]:bg-gray-200 data-[haserrors=true]:text-destructive p-4 rounded-t-md transition-colors"
+            >
+              {`Datos Generales ${
+                erroresGenerales > 0 ? `(${erroresGenerales} errores)` : ""
+              }`}
+            </AccordionTrigger>
+            <AccordionContent className="rounded-b-md bg-muted/50 p-4">
+              <DatosGeneralesContratacionForm
+                form={datosGeneralesForm}
+                onSubmitCon={onSubmitGe}
+              ></DatosGeneralesContratacionForm>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="datos-cumplimiento">
+            <AccordionTrigger
+              data-haserrors={erroresCumplimiento > 0}
+              className="[&[data-state=open]]:bg-gray-200 data-[haserrors=true]:text-destructive p-4 rounded-t-md transition-colors"
+            >
+              {`Datos Cumplimiento ${
+                erroresCumplimiento > 0
+                  ? `(${erroresCumplimiento} errores)`
+                  : ""
+              }`}
+            </AccordionTrigger>
+            <AccordionContent className="rounded-b-md bg-muted/50 p-4">
+              <DatosFianzaCumplimientosForm
+                form={datosCumplimientoForm}
+                onSubmit={onSubmitCum}
+              ></DatosFianzaCumplimientosForm>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="datos-ocultos">
+            <AccordionTrigger
+              data-haserrors={erroresOculto > 0}
+              className="[&[data-state=open]]:bg-gray-200 data-[haserrors=true]:text-destructive p-4 rounded-t-md transition-colors"
+            >
+              {`Datos Ocultos ${
+                erroresOculto > 0 ? `(${erroresOculto} errores)` : ""
+              }`}
+            </AccordionTrigger>
+            <AccordionContent className="rounded-b-md bg-muted/50 p-4">
+              <DatosFianzaOcultosForm
+                form={datosOcultosForm}
+                onSubmit={onSubmitOcu}
+              ></DatosFianzaOcultosForm>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="datos-anticipos">
+            <AccordionTrigger
+              data-haserrors={erroresAnticipo > 0}
+              className="[&[data-state=open]]:bg-gray-200 data-[haserrors=true]:text-destructive p-4 rounded-t-md transition-colors"
+            >
+              {`Datos Anticipo ${
+                erroresAnticipo > 0 ? `(${erroresAnticipo} errores)` : ""
+              }`}
+            </AccordionTrigger>
+            <AccordionContent className="rounded-b-md bg-muted/50 p-4">
+              <DatosFianzaAnticiposForm
+                form={datosAnticipoForm}
+                onSubmit={onSubmitAntic}
+              ></DatosFianzaAnticiposForm>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </main>
+    </Layout>
+  );
+}
