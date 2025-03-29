b63688d1fe1674c5d57afd7062e99e670405481c
Kartleth <80503620+Kartleth@users.noreply.github.com>
2025-03-28 21:46:57 -0700

    Lógica para borrar Fianza de cumplimiento

--- src/components/data-tables/data-table-fianza-cumplimineto.tsx
+++ src/components/data-tables/data-table-fianza-cumplimineto.tsx
@@ -181,7 +181,7 @@ export const getColumns = (contratoId: string): ColumnDef<Fianza>[] => [
       const Fianza = row.original;
       const queryClient = useQueryClient();
       const deleteFianza = useMutation(async () => {
-        const res = await fetch(`http://localhost:3001/fianza/`, {
+        const res = await fetch(`http://localhost:3001/fianza/contrato/${contratoId}/${Fianza.idfianza}`, {
           method: "delete",
           headers: {
             "Content-Type": "application/json",
