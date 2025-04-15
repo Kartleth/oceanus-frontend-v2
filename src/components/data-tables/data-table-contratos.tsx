4e4a2e878ced76f36e230f573694109b4ee834a9
ilsespinoza <104655979+ilsespinoza@users.noreply.github.com>
2025-04-15 15:56:15 -0700

    contratos funcion
    pdf
    excel
    imprimir

--- src/components/data-tables/data-table-contratos.tsx
+++ src/components/data-tables/data-table-contratos.tsx
@@ -54,6 +54,10 @@ import { Link } from "react-router-dom";
 import { Contrato } from "@/modelos/datosContratos";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import * as XLSX from "xlsx";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { Skeleton } from "../ui/skeleton";
+import ErrorComponent from "../error-component";
 
 export const columns: ColumnDef<Contrato>[] = [
   {
@@ -370,16 +374,16 @@ export function DataTableContratos() {
       "Fin de contrato": contrato.fincontrato,
       Numero_de_contrato: contrato.numerocontrato,
       montocontrato: contrato.montocontrato,
-      facturas: contrato.facturas,
-      idcontratofuente: contrato.idcontratofuente,
+      // facturas: contrato.facturas,
+      // idcontratofuente: contrato.idcontratofuente,
       Direccion: contrato.direccion,
       anticipocontrato: contrato.anticipocontrato,
-      convenios: contrato.convenios,
-      personalcontrato: contrato.personalcontrato,
-      fianzacumplimiento: contrato.fianzaCumplimiento,
-      fianzaanticipo: contrato.fianzaAnticipo,
-      fianzaoculto: contrato.fianzaOculto,
-      datospersonal: contrato.datosPersonal,
+      // convenios: contrato.convenios,
+      // personalcontrato: contrato.personalcontrato,
+      // fianzacumplimiento: contrato.fianzaCumplimiento,
+      // fianzaanticipo: contrato.fianzaAnticipo,
+      // fianzaoculto: contrato.fianzaOculto,
+      // datospersonal: contrato.datosPersonal,
     }));
 
     //Exportar a Excel
@@ -401,14 +405,281 @@ export function DataTableContratos() {
       { header: "Nombre", key: "nombrecontrato" },
       { header: "Inicio de contrato", key: "iniciocontrato" },
       { header: "Fin de contrato", key: "fincontrato" },
+      { header: "Numero_de_contrato", key: "numerocontrato" },
       { header: "montocontrato", key: "montocontrato" },
-      { header: "Fecha de Ingreso", key: "fechaingreso" },
-      { header: "Estado", key: "estado" },
-      { header: "Tipo de contrato", key: "tipocontrato" },
-      { header: "Inicio del Contrato", key: "iniciocontrato" },
-      { header: "Fin del Contrato", key: "fincontrato" },
+      { header: "Direccion", key: "direccion" },
+      { header: "anticipocontrato", key: "anticipocontrato" },
     ];
+    // Función para mapear los datos a cada bloque
+    const generarDatosBloque = (
+      contratos: Contrato[],
+      campos: { header: string; key: string }[]
+    ) => {
+      return contratos.map((contrato) => {
+        const bloque: Record<string, string> = {};
+        campos.forEach(({ header, key }) => {
+          const valor =
+            key
+              .split(".")
+              .reduce((acc, curr) => acc?.[curr], contrato as any) || "N/A";
+          bloque[header] = valor;
+        });
+        return bloque;
+      });
+    };
+    // Generar los datos para cada bloque
+    const datosBloque1 = generarDatosBloque(contratos, camposBloque1);
+
+    // Crear un nuevo documento PDF
+    const doc = new jsPDF({
+      orientation: "landscape",
+      unit: "mm",
+      format: "a4",
+    });
+
+    const leftMargin = 10;
+    const rightMargin = 10;
+    const pageWidth = doc.internal.pageSize.width;
+
+    // Logo más pequeño
+    const logoWidth = 12;
+    const logoHeight = 12;
+    const logoY = 7;
+    doc.addImage(logoPath, "PNG", leftMargin, logoY, logoWidth, logoHeight);
+
+    // Título derecho
+    const text2 = "OCEANUS SUPERVISION Y PROYECTOS";
+    const fontSize = 11;
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(fontSize);
+
+    const textWidth2 =
+      (doc.getStringUnitWidth(text2) * fontSize) / doc.internal.scaleFactor;
+    const xPosition2 = pageWidth - rightMargin - textWidth2;
+    const yPosition2 = logoY + logoHeight / 2 + 1; // Alineado verticalmente al centro del logo
+    doc.text(text2, xPosition2, yPosition2);
+
+    // Línea azul decorativa
+    const lineY = 25;
+    doc.setDrawColor(41, 128, 185);
+    doc.setLineWidth(0.5);
+    doc.line(leftMargin, lineY, pageWidth - rightMargin, lineY);
+
+    // Título principal
+    const text1 = "DATOS DE PERSONALES GENERALES";
+    const textWidth1 =
+      (doc.getStringUnitWidth(text1) * fontSize) / doc.internal.scaleFactor;
+    const xPosition1 = (pageWidth - textWidth1) / 2;
+    const yPosition1 = lineY + 8;
+    doc.text(text1, xPosition1, yPosition1);
+
+    // Ajusta la posición del texto
+
+    const generarTabla = (datos: any[], startY: number) => {
+      autoTable(doc, {
+        head: [Object.keys(datos[0])], // Cabecera de la tabla
+        body: datos.map((persona) => Object.values(persona)), // Filas de la tabla
+        startY, // Comienza en la posición Y proporcionada
+        theme: "grid",
+        headStyles: {
+          fillColor: [41, 128, 185], // Azul intenso para la cabecera
+          textColor: [255, 255, 255], // Texto blanco
+          fontSize: 7, // Tamaño de la fuente reducido
+          fontStyle: "bold",
+        },
+        bodyStyles: {
+          fontSize: 6, // Tamaño de la fuente reducido
+          cellPadding: 2, // Espaciado reducido dentro de las celdas
+          textColor: [51, 51, 51], // Color gris oscuro para el texto
+        },
+        alternateRowStyles: {
+          fillColor: [241, 245, 249], // Azul claro para filas alternas
+        },
+        styles: {
+          overflow: "linebreak",
+          cellWidth: "auto", // Ajuste automático del ancho de las celdas
+          lineColor: [200, 200, 200], // Bordes grises claros
+          lineWidth: 0.1, // Grosor de los bordes
+        },
+        columnStyles: {
+          0: { cellWidth: 15 }, // Ancho específico para la primera columna
+          1: { cellWidth: "auto" }, // Ancho automático para otras columnas
+        },
+        margin: { top: 25 },
+        pageBreak: "auto", // El salto de página se maneja automáticamente
+      });
+    };
+    generarTabla(datosBloque1, 40);
+    doc.addPage();
+    doc.save("Contratos.pdf");
   };
+
+  //Imprimir datos
+  const logoPath = "src/assets/oceanus-logo-3.png";
+  const handlePrint = (contratos: Contrato[]) => {
+    if (!contratos || contratos.length === 0) {
+      alert("No hay datos disponibles para imprimir.");
+      return;
+    }
+
+    const doc = new jsPDF({
+      orientation: "landscape",
+      unit: "mm",
+      format: "a4",
+    });
+
+    const leftMargin = 10;
+    const rightMargin = 10;
+    const pageWidth = doc.internal.pageSize.width;
+
+    // Logo más pequeño
+    const logoWidth = 12;
+    const logoHeight = 12;
+    const logoY = 7;
+    doc.addImage(logoPath, "PNG", leftMargin, logoY, logoWidth, logoHeight);
+
+    // Título derecho
+    const text2 = "OCEANUS SUPERVISION Y PROYECTOS";
+    const fontSize = 11;
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(fontSize);
+
+    const textWidth2 =
+      (doc.getStringUnitWidth(text2) * fontSize) / doc.internal.scaleFactor;
+    const xPosition2 = pageWidth - rightMargin - textWidth2;
+    const yPosition2 = logoY + logoHeight / 2 + 1; // Alineado verticalmente al centro del logo
+    doc.text(text2, xPosition2, yPosition2);
+
+    // Línea azul decorativa
+    const lineY = 25;
+    doc.setDrawColor(41, 128, 185);
+    doc.setLineWidth(0.5);
+    doc.line(leftMargin, lineY, pageWidth - rightMargin, lineY);
+
+    // Título principal
+    const text1 = "DATOS DE PERSONALES GENERALES";
+    const textWidth1 =
+      (doc.getStringUnitWidth(text1) * fontSize) / doc.internal.scaleFactor;
+    const xPosition1 = (pageWidth - textWidth1) / 2;
+    const yPosition1 = lineY + 8;
+    doc.text(text1, xPosition1, yPosition1);
+
+    // Ajusta la posición del texto
+
+    const generarTabla = (datos: any[], startY: number) => {
+      autoTable(doc, {
+        head: [Object.keys(datos[0])], // Cabecera de la tabla
+        body: datos.map((persona) => Object.values(persona)), // Filas de la tabla
+        startY, // Comienza en la posición Y proporcionada
+        theme: "grid",
+        headStyles: {
+          fillColor: [41, 128, 185], // Azul intenso para la cabecera
+          textColor: [255, 255, 255], // Texto blanco
+          fontSize: 7, // Tamaño de la fuente reducido
+          fontStyle: "bold",
+        },
+        bodyStyles: {
+          fontSize: 6, // Tamaño de la fuente reducido
+          cellPadding: 2, // Espaciado reducido dentro de las celdas
+          textColor: [51, 51, 51], // Color gris oscuro para el texto
+        },
+        alternateRowStyles: {
+          fillColor: [241, 245, 249], // Azul claro para filas alternas
+        },
+        styles: {
+          overflow: "linebreak",
+          cellWidth: "auto", // Ajuste automático del ancho de las celdas
+          lineColor: [200, 200, 200], // Bordes grises claros
+          lineWidth: 0.1, // Grosor de los bordes
+        },
+        columnStyles: {
+          0: { cellWidth: 15 }, // Ancho específico para la primera columna
+          1: { cellWidth: "auto" }, // Ancho automático para otras columnas
+        },
+        margin: { top: 25 },
+        pageBreak: "auto", // El salto de página se maneja automáticamente
+      });
+    };
+
+    const camposBloque1 = [
+      { header: "ID", key: "idcontrato" },
+      { header: "Nombre", key: "nombrecontrato" },
+      { header: "Inicio de contrato", key: "iniciocontrato" },
+      { header: "Fin de contrato", key: "fincontrato" },
+      { header: "Numero_de_contrato", key: "numerocontrato" },
+      { header: "montocontrato", key: "montocontrato" },
+      { header: "Direccion", key: "direccion" },
+      { header: "anticipocontrato", key: "anticipocontrato" },
+    ];
+
+    // Función para mapear los datos a cada bloque
+    const generarDatosBloque = (
+      contratos: Contrato[],
+      campos: { header: string; key: string }[]
+    ) => {
+      return contratos.map((contrato) => {
+        const bloque: Record<string, string> = {};
+        campos.forEach(({ header, key }) => {
+          const valor =
+            key
+              .split(".")
+              .reduce((acc, curr) => acc?.[curr], contrato as any) || "N/A";
+          bloque[header] = valor;
+        });
+        return bloque;
+      });
+    };
+
+    const datosBloque1 = generarDatosBloque(contratos, camposBloque1);
+
+    generarTabla(datosBloque1, 40);
+    // Convertir el PDF a un blob
+    const pdfBlob = doc.output("blob");
+
+    // Crear una URL temporal para el Blob
+    const pdfUrl = URL.createObjectURL(pdfBlob);
+
+    // Abrir el PDF en una nueva ventana para imprimir
+    const printWindow = window.open(pdfUrl, "_blank");
+
+    if (printWindow) {
+      printWindow.onload = () => {
+        printWindow.print();
+      };
+    } else {
+      alert("No se pudo abrir la ventana para imprimir.");
+    }
+  };
+  /* AGREGAR DISEÑO AL APARTADO DE CARGANDO*/
+  if (contratosQuery.isLoading) {
+    return (
+      <div className="w-full space-y-2">
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className="rounded-md border p-4">
+            <div className="flex space-x-4">
+              <Skeleton className="h-6 w-1/4" />
+              <Skeleton className="h-6 w-1/3" />
+              <Skeleton className="h-6 w-1/5" />
+              <Skeleton className="h-6 w-1/6" />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  // Si ocurre un error, lo mostramos
+  if (contratosQuery.error instanceof Error) {
+    return (
+      <div className="w-full h-1/2 text-center">
+        <ErrorComponent></ErrorComponent>
+        <div className="mt-8 text-2xl">
+          Error: {contratosQuery.error.message}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full">
       <div className="flex items-center py-4">
@@ -433,8 +704,16 @@ export function DataTableContratos() {
             >
               Exportar a Excel
             </DropdownMenuItem>
-            <DropdownMenuItem>Exportar a PDF</DropdownMenuItem>
-            <DropdownMenuItem>Imprimir</DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => exportToPDF(contratosQuery.data || [])}
+            >
+              Exportar a PDF
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => handlePrint(contratosQuery.data || [])}
+            >
+              Imprimir
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
 
